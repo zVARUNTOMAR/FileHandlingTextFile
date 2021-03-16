14680b68fc60338efe8f57bddd4e9cade5fa1ab7
Varun Tomar <varuntomar3301@outlook.com>
2021-03-16 22:23:40 +0530

    intial commit

---
+++ FileHandlingDemo/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandlingDemo
+{
+    [Serializable]
+    public class Customer
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string city { get; set; }
+
+       public Customer() { 
+            
+        }
+
+        public Customer(string name, int age, string city) {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+    }
+}
---
+++ FileHandlingDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FileHandlingDemo
+{
+    internal class Tutorial
+    {
+        public static void Main(string[] args)
+        {
+            FileStream stream = new FileStream(@"D:\Games\Customers.txt", FileMode.OpenOrCreate);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            Customer NewCustomer = new Customer("varun",23,"delhi");
+
+            formatter.Serialize(stream, NewCustomer);
+            //end
+
+            //deseralization start
+            stream.Position = 0;
+
+            Customer AgainNewCustomer = (Customer)formatter.Deserialize(stream);
+
+            Console.WriteLine("Name : {0}",AgainNewCustomer.name);
+            Console.WriteLine("Age : {0}", AgainNewCustomer.age);
+            Console.WriteLine("City : {0}", AgainNewCustomer.city);
+
+            stream.Close();
+        }
+    }
+}
